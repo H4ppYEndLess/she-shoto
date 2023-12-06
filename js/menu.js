0db5c398c7a232b7247b9ba9270318b25379262c
H4ppy <95917527+H4ppYEndLess@users.noreply.github.com>
2023-12-06 10:31:48 +0200

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -8,6 +8,7 @@
     function openMenu(){
       refs.opened.classList.toggle('section_menu')
       refs.opened.classList.toggle("menu_active") 
+      refs.menu.classList.toggle("burger_opened")
       document.body.classList.toggle("lock")
     }
     refs.menu.addEventListener("click", openMenu);
