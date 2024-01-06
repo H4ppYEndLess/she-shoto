ea1c59151d32d476dcaee04c38e68ea25d6979b0
H4ppy <95917527+H4ppYEndLess@users.noreply.github.com>
2024-01-06 12:10:58 +0200

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,19 @@
 (() => {
-    const refs = {
-        menu:document.querySelector('[open-menu]'),
-        opened:document.querySelector('[menu]'),
-        back:document.querySelector('backdrop'),
-        close:document.querySelector('[close-menu]')
-    };
-    function openMenu(){
-      refs.opened.classList.toggle('section_menu')
-      refs.opened.classList.toggle("menu_active") 
-      refs.menu.classList.toggle("burger_opened")
-      document.body.classList.toggle("lock")
-    }
-    refs.menu.addEventListener("click", openMenu);
-    refs.close.addEventListener("click", openMenu);
-  })();
+  const refs = {
+      links:document.querySelectorAll('.menu__link'),
+      menu:document.querySelector('[open-menu]'),
+      opened: document.querySelector('[menu]'),
+      back: document.querySelector('backdrop'),
+  };
+  function openMenu(){
+    refs.opened.classList.toggle('section_menu')
+    refs.opened.classList.toggle("menu_active") 
+    refs.menu.classList.toggle("burger_opened")
+    document.body.classList.toggle("lock")
+  }
+  refs.menu.addEventListener("click", openMenu);
+
+  refs.links.forEach(element => {
+    element.addEventListener("click", openMenu);
+});
+})();
